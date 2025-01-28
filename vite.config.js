bf02aa1bd63908986c6918f0589656c2939f2f87
Darko Cvetkovic <darkocvetkovic@Darkos-MacBook-Pro.local>
2025-01-28 12:33:21 +0100

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import Components from 'unplugin-vue-components/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: "/resume/",
   plugins: [
     vue(),
     Components({
